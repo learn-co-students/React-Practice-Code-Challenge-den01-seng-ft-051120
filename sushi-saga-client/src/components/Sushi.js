a65475f63b0332d0a477a294d01e9639c9ab13a5
Bryce Kennedy <btken88@gmail.com>
2020-07-25 16:29:41 -0600

    Done.

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,21 +1,18 @@
 import React, { Fragment } from 'react'
 
-const Sushi = (props) => {
+const Sushi = ({ sushi, eatSushi }) => {
   return (
     <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          false ?
-            null
-          :
-            <img src={/* Give me an image source! */ } width="100%" />
-        }
-      </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
+      {sushi.name ?
+        (<Fragment>
+          <div className="plate" onClick={() => eatSushi(sushi)}>
+            <img src={sushi.img_url} width="100%" alt={sushi.name} />
+          </div>
+          <h4 className="sushi-details">
+            {sushi.name} - ${sushi.price}
+          </h4>
+        </Fragment>)
+        : null}
     </div>
   )
 }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
+const SushiContainer = ({ sushiSelection, eatSushi, moreSushi }) => {
+  const sushiBelt = sushiSelection.map(sushi => {
+    return <Sushi key={sushi.id} sushi={sushi} eatSushi={eatSushi} />
+  })
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {sushiBelt}
+        <MoreButton moreSushi={moreSushi} />
       </div>
     </Fragment>
   )
