50cc76ef01a3ae2bd7e932f51b4d588e6d73eae6
Kyle <kpete2017@gmail.com>
2020-07-26 14:36:55 -0600

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,65 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    allSushi: [],
+    servedSushi: [],
+    plates: [],
+    currentListIndex: 0,
+    cash: 100
+  }
+  
+  componentDidMount() {
+    fetch(API)
+      .then(results => results.json())
+      .then(sushiList => this.setFirstSushiRender(sushiList))
+  }
+
+  setFirstSushiRender = (sushiList) => { this.setAllSushiState(sushiList), this.setServedSushiState(this.state.currentListIndex)}
+
+  setAllSushiState = (sushiList) => { this.setState({ allSushi: sushiList }) }
+
+  setServedSushiState = (index) => {
+    console.log(index)
+    let temporaryArray = this.state.allSushi
+    let fourSushi = temporaryArray.slice(index, index + 4)
+    this.setState({ servedSushi: fourSushi }) 
+  }
+
+  eatenSushi = (sushi) => {
+    if(this.state.cash - sushi.price > 0) {
+      let temporaryArray = this.state.plates
+      temporaryArray.push(sushi)
+      this.setState({
+        plates: temporaryArray,
+        cash: this.state.cash - sushi.price
+      })
+    } else {
+      alert("Can not afford this sushi")
+    }
+  }
+
+  moreSushi = () => {
+    let newValue = this.state.currentListIndex + 4
+    if(newValue === 100) {
+      newValue = 0
+      this.setState({
+        currentListIndex: newValue
+      })
+
+    } else {
+      this.setState({
+        currentListIndex: newValue
+      })
+    }
+    this.setServedSushiState(newValue)
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer servedSushi={this.state.servedSushi} eatenSushi={this.eatenSushi} moreSushi={this.moreSushi}/>
+        <Table cash={this.state.cash} plates={this.state.plates}/>
       </div>
     );
   }
